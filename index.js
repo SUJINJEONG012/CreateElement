299f28a56e7a84b30cf9d0ddbfd2059b5c247795
SUJINJEONG012 <peekaboo32@naver.com>
2022-06-22 16:01:21 +0900

    js파일 분리

---
+++ index.js
@@ -0,0 +1,8 @@
+function getValue() {
+    let el = document.createElement("li");
+
+    let arrayList = document.getElementById("itemName").value;
+
+    console.log(arrayList);
+    document.getElementById("getList").appendChild(el).innerText = arrayList;
+} s
