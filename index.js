7076f40e46e9145bc00bd4e2a3076954ff07abf9
SUJINJEONG012 <peekaboo32@naver.com>
2022-06-22 16:26:16 +0900

    수정

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 function getValue() {
     let el = document.createElement("li");
-
     let arrayList = document.getElementById("itemName").value;
 
     console.log(arrayList);
     document.getElementById("getList").appendChild(el).innerText = arrayList;
-} s
+
+}
+
